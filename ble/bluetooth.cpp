a7dddbf9aa14f68c2c2807bc4730abbb1f927078
Alexey Eremin <a.eremin.msu@gmail.com>
2019-09-21 14:06:03 +0200

    Remove unneeded dependencies

--- ble/bluetooth.cpp
+++ ble/bluetooth.cpp
@@ -1,24 +1,13 @@
 #include "bluetooth.h"
 
-#include "app_error.h"
 #include "app_timer.h"
-#include "ble.h"
+#include "app_error.h"
 #include "ble_advdata.h"
 #include "ble_conn_params.h"
-#include "ble_err.h"
-#include "ble_hci.h"
 #include "ble_lbs.h"
-#include "ble_srv_common.h"
-#include "boards.h"
-#include "nordic_common.h"
-#include "nrf.h"
 #include "nrf_ble_gatt.h"
 #include "nrf_sdh.h"
-#include "nrf_sdh_ble.h"
-
 #include "nrf_log.h"
-#include "nrf_log_ctrl.h"
-#include "nrf_log_default_backends.h"
 
 #define DEVICE_NAME "Nordic_Blinky" /**< Name of device. Will be included in the advertising data. */
 
