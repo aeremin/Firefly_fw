bec7cfc36aa20cdb5a37e9ec263927c54d601d3b
Alexey Eremin <a.eremin.msu@gmail.com>
2019-09-21 13:05:58 +0200

    Remove unneeded "extern C"

--- ble/ble.h
+++ ble/ble.h
@@ -1,16 +1,8 @@
 #ifndef BLE_H
 #define BLE_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 typedef void (*BleCallback)(bool);
 
 void InitBle(BleCallback callback);
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif // BLE_H
