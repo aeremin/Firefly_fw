51456fdfcf1f9625724328a638c3e78433d8f50c
Alexey Eremin <a.eremin.msu@gmail.com>
2019-09-29 16:32:26 +0200

    Remove unused code

--- testing/test.ts
+++ testing/test.ts
@@ -1,8 +1,5 @@
-import { Adapter, AdapterFactory, Characteristic, ConnectionOptions,
-  Descriptor, Device, ScanParameters, Service } from "pc-ble-driver-js";
 import { BluetoothLowEnergy } from "./ble";
 
-const gAdapter = AdapterFactory.getInstance().createAdapter("v3", "COM18", "");
 
 const LED_BUTTON_SERVICE_UUID = "000015231212efde1523785feabcd123";
 const BUTTON_CHARACTERISTIC_UUID = "000015241212efde1523785feabcd123";
